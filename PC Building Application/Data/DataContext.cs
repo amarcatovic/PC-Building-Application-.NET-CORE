b0b037ff54ff018d4d238218bc8604bb8cf8d95b
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-09 17:11:22 +0200

    Adding DbContext Class

---
+++ PC Building Application/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
