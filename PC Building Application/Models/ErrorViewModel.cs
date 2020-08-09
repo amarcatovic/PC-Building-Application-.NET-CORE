5eb9042960cc6eab4f4846ea85604fd59fbd1128
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-09 17:08:45 +0200

    Add project files.

---
+++ PC Building Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PC_Building_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
