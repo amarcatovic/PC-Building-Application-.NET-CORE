ab115501bee9da2f2b8597757e139652c4ed502b
Amar Ćatović <amar_catovic@hotmail.com>
2020-08-09 17:35:58 +0200

    Added User Repository

---
+++ PC Building Application/Data/Repositories/Interfaces/IUserRepo.cs
@@ -0,0 +1,17 @@
+﻿using PC_Building_Application.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories.Interfaces
+{
+    public interface IUserRepo
+    {
+        Task<User> Login(string username, string password);
+
+        Task<User> Register(User newUser, string password);
+
+        Task<bool> UserExsists(string username);
+    }
+}
