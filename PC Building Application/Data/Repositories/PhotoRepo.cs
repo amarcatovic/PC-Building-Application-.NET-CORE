768d2911b4b47495703ecb934b8d55d9cdda651e
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-05 14:00:08 +0200

    Add Cloudinary Settings

--- PC Building Application/Data/Repositories/PhotoRepo.cs
+++ PC Building Application/Data/Repositories/PhotoRepo.cs
@@ -1,5 +1,8 @@
-﻿using PC_Building_Application.Data.Models;
+﻿using CloudinaryDotNet;
+using Microsoft.Extensions.Options;
+using PC_Building_Application.Data.Models;
 using PC_Building_Application.Data.Repositories.Interfaces;
+using PC_Building_Application.Helper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +13,31 @@ namespace PC_Building_Application.Data.Repositories
     public class PhotoRepo : IPhotoRepo
     {
         private readonly DataContext _context;
+        private readonly IOptions<CloudinarySettings> _cloudinarySettings;
+        private Cloudinary _cloudinary;
 
-        public PhotoRepo(DataContext context)
+        public PhotoRepo(DataContext context, IOptions<CloudinarySettings> cloudinarySettings)
         {
             _context = context;
+            _cloudinarySettings = cloudinarySettings;
+
+            Account account = new Account(
+                _cloudinarySettings.Value.CloudName,
+                _cloudinarySettings.Value.APIKey,
+                _cloudinarySettings.Value.APISecret
+            );
+
+            _cloudinary = new Cloudinary(account);
         }
 
         public async Task AddPhoto(Photo photo)
         {
             await _context.AddAsync(photo);
+        }
+
+        public Task<Photo> AddPhotoForComponent(int componentId)
+        {
+            
         }
 
         public async Task<int> Done()
