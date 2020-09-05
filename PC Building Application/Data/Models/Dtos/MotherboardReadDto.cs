573b1c5e67eaef5a7b420eac5831a9d6654f7edf
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-05 14:27:38 +0200

    Created

---
+++ PC Building Application/Data/Models/Dtos/MotherboardReadDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos
+{
+    public class MotherboardReadDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Released { get; set; }
+        public SocketType SocketType { get; set; }
+        public int MaxMemmoryFreq { get; set; }
+        public string MemoryType { get; set; }
+        public int NoOfM2Slots { get; set; }
+        public bool HasRGB { get; set; }
+        public int NoOfPCIeSlots { get; set; }
+        public int NoOfRAMSlots { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+        public PhotoReturnDto Photo { get; set; }
+    }
+}
