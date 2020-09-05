cf13cee1bfb794b996a3f9cb2639bb7ba8c4b3ce
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-05 16:22:48 +0200

    Create maping profile

---
+++ PC Building Application/Profiles/MotherboardProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using PC_Building_Application.Data.Models;
+using PC_Building_Application.Data.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Profiles
+{
+    public class MotherboardProfiles : Profile
+    {
+        public MotherboardProfiles()
+        {
+            CreateMap<Motherboard, MotherboardReadDto>();
+            CreateMap<MotherboardCreateDto, Motherboard>();
+        }
+    }
+}
