09bb4e8bbbada60ff90b39941c4814709b83b0e3
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-05 14:22:13 +0200

    .

---
+++ PC Building Application/Data/Repositories/Interfaces/IMotherboardRepo.cs
@@ -0,0 +1,16 @@
+﻿using PC_Building_Application.Data.Models;
+using PC_Building_Application.Data.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories.Interfaces
+{
+    public interface IMotherboardRepo
+    {
+        Task<Motherboard> GetAllMotherboards();
+        Task<Motherboard> GetMotherboardById(int id);
+        Task<Motherboard> CreateMotherboard(Motherboard mobo, PhotoToCreateDto photo);
+    }
+}
