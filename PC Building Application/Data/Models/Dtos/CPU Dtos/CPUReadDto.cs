35d70635f3b0e7c5b0d27043a174b1a2d9d2b17f
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 13:43:34 +0200

    DTO for returning CPU Data

---
+++ PC Building Application/Data/Models/Dtos/CPU Dtos/CPUReadDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.CPU_Dtos
+{
+    public class CPUReadDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Released { get; set; }
+        public string SocketType { get; set; }
+        public string Clockspeed { get; set; }
+        public string TurboSpeed { get; set; }
+        public int NoOfCores { get; set; }
+        public int SingleThreadRating { get; set; }
+        public string Manufacturer { get; set; }
+        public PhotoReturnDto Photo { get; set; }
+    }
+}
