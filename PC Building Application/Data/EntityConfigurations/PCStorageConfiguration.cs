3cdfc583718c208ed998434654346cfd04b404f4
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 19:14:56 +0200

    add join table entity config for pc and storages

---
+++ PC Building Application/Data/EntityConfigurations/PCStorageConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.EntityConfigurations
+{
+    public class PCStorageConfiguration : IEntityTypeConfiguration<PCStorage>
+    {
+        public void Configure(EntityTypeBuilder<PCStorage> builder)
+        {
+            builder.HasKey(ps => new { ps.StorageId, ps.PCId });
+
+            builder.HasOne(ps => ps.PC)
+                .WithMany(pc => pc.PCStorages)
+                .HasForeignKey(ps => ps.PCId);
+
+            builder.HasOne(ps => ps.Storage)
+                .WithMany(s => s.PCStorages)
+                .HasForeignKey(ps => ps.StorageId);
+        }
+    }
+}
