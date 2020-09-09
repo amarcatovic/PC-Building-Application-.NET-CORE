8e0c07f912089050810c2e55233c7f66df7a1609
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 11:21:12 +0200

    Create interface repo

---
+++ PC Building Application/Data/Repositories/Interfaces/ISocketTypeRepo.cs
@@ -0,0 +1,13 @@
+﻿using PC_Building_Application.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories.Interfaces
+{
+    public interface ISocketTypeRepo
+    {
+        Task<IEnumerable<SocketType>> GetSocketTypes();
+        Task<SocketType> GetSingleSocketTypeById(int id);
+        Task CreateSocketType(SocketType socketType);
+    }
+}
