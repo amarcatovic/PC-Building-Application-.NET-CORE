9221fd901226d939727080a08d6a8bf73efe9e99
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 13:43:39 +0200

    Add CPU profiles

---
+++ PC Building Application/Profiles/CPUProfiles.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using PC_Building_Application.Data.Models;
+using PC_Building_Application.Data.Models.Dtos.CPU_Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Profiles
+{
+    public class CPUProfiles : Profile
+    {
+        public CPUProfiles()
+        {
+            CreateMap<CPU, CPUReadDto>()
+                .ForMember(dest => dest.SocketType, opt => opt.MapFrom(src => src.SocketType.Name))
+                .ForMember(dest => dest.Manufacturer, opt => opt.MapFrom(src => src.Manufacturer.Name));
+            CreateMap<CPUCreateDto, CPU>();
+        }
+    }
+}
