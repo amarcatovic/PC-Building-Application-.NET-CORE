2587b2a1d76fb8c93665c4b64cdf5b1e1884f123
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 13:56:11 +0200

    Add Migration for Photo to CPU

---
+++ PC Building Application/Migrations/20200909115547_AddPhotoToCPUEntity.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PC_Building_Application.Migrations
+{
+    public partial class AddPhotoToCPUEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PhotoId",
+                table: "CPUs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CPUs_PhotoId",
+                table: "CPUs",
+                column: "PhotoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CPUs_Photos_PhotoId",
+                table: "CPUs",
+                column: "PhotoId",
+                principalTable: "Photos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CPUs_Photos_PhotoId",
+                table: "CPUs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CPUs_PhotoId",
+                table: "CPUs");
+
+            migrationBuilder.DropColumn(
+                name: "PhotoId",
+                table: "CPUs");
+        }
+    }
+}
