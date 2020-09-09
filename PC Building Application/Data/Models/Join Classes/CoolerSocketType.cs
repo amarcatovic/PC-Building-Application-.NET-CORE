b024f5fcfcfaa7e7d6adb9c245f6685d87cce011
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 18:11:18 +0200

    join class

---
+++ PC Building Application/Data/Models/Join Classes/CoolerSocketType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Join_Classes
+{
+    public class CoolerSocketType
+    {
+        public Cooler Cooler { get; set; }
+        public int CoolerId { get; set; }
+        public SocketType SocketType { get; set; }
+        public int SocketTypeId { get; set; }
+    }
+}
