17a1c95afe17e8376eda18b3d138c0f2155b338d
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 18:10:45 +0200

    add gpu entity configuration

---
+++ PC Building Application/Data/EntityConfigurations/GPUConfiguration.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PC_Building_Application.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.EntityConfigurations
+{
+    public class GPUConfiguration : IEntityTypeConfiguration<GPU>
+    {
+        public void Configure(EntityTypeBuilder<GPU> builder)
+        {
+            builder.HasKey(gpu => gpu.Id);
+
+            builder.Property(gpu => gpu.Name)
+                .IsRequired();
+
+            builder.Property(gpu => gpu.MemoryType)
+                .IsRequired();
+
+            builder.Property(gpu => gpu.Released)
+                .IsRequired();
+
+            builder.Property(gpu => gpu.PCIPort)
+                .IsRequired();
+
+            builder.Property(gpu => gpu.MemoryType)
+                .IsRequired();
+
+            builder.Property(gpu => gpu.VRAM)
+                .IsRequired();
+
+            builder.Property(gpu => gpu.NoOfHDMIPorts)
+                .IsRequired();
+
+            builder.Property(gpu => gpu.NoOfDisplayPorts)
+                .IsRequired();
+
+            builder.Property(gpu => gpu.HasDVI)
+                .IsRequired();
+
+            builder.Property(gpu => gpu.HasVGA)
+                .IsRequired();
+
+            builder.Property(gpu => gpu.Price)
+                .IsRequired();
+
+            builder.HasOne(gpu => gpu.Photo)
+                .WithMany(p => p.GPUs)
+                .HasForeignKey(gpu => gpu.PhotoId);
+
+            builder.HasOne(gpu => gpu.Manufacturer)
+                .WithMany(m => m.GPUs)
+                .HasForeignKey(gpu => gpu.ManufacturerId);
+        }
+    }
+}
