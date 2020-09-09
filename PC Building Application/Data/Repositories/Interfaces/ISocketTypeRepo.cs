88633e4edab01abd6408a5cbb0556d00e84cd0a3
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 11:28:13 +0200

    Add Done Method

--- PC Building Application/Data/Repositories/Interfaces/ISocketTypeRepo.cs
+++ PC Building Application/Data/Repositories/Interfaces/ISocketTypeRepo.cs
@@ -9,5 +9,6 @@ namespace PC_Building_Application.Data.Repositories.Interfaces
         Task<IEnumerable<SocketType>> GetSocketTypes();
         Task<SocketType> GetSingleSocketTypeById(int id);
         Task CreateSocketType(SocketType socketType);
+        bool Done();
     }
 }
