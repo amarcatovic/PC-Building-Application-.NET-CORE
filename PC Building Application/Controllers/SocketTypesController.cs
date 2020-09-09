4286ef6df8edc65ecf5b2cb379f6d928c88d5766
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 11:46:20 +0200

    Add controller

---
+++ PC Building Application/Controllers/SocketTypesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PC_Building_Application.Data.Repositories.Interfaces;
+
+namespace PC_Building_Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocketTypesController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ISocketTypeRepo _repo;
+
+        public SocketTypesController(IMapper mapper, ISocketTypeRepo repo)
+        {
+            _mapper = mapper;
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetSocketTypes()
+        {
+            var socketTypesFromDb = await _repo.GetSocketTypes();
+
+
+
+            return Ok(socketTypesFromDb);
+        }
+    }
+}
