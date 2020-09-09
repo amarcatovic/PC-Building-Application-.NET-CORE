03adf962ae09cc43d67b6b544f383b578ec6a37d
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 18:11:34 +0200

    add gpu and cooler

--- PC Building Application/Data/Models/PC.cs
+++ PC Building Application/Data/Models/PC.cs
@@ -18,6 +18,10 @@ namespace PC_Building_Application.Data.Models
         public int CPUId { get; set; }
         public User User { get; set; }
         public string UserId { get; set; }
+        public GPU GPU { get; set; }
+        public int GPUId { get; set; }
+        public Cooler Cooler { get; set; }
+        public int CoolerId { get; set; }
         public ICollection<PCRAM> PCRAMs { get; set; }
     }
 }
