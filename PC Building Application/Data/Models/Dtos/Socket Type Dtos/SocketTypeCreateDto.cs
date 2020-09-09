14d0c8a3b863dd7b3263ad1ac46a5d60d22bac2f
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 11:34:47 +0200

    Socket Type Create Dto

---
+++ PC Building Application/Data/Models/Dtos/Socket Type Dtos/SocketTypeCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Socket_Type_Dtos
+{
+    public class SocketTypeCreateDto
+    {
+        public string Name { get; set; }
+    }
+}
