8ff0631e642d1e6163d579b6c27d803788d97e91
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 17:15:11 +0200

    Add RAM Profiles

---
+++ PC Building Application/Profiles/RAMProfiles.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using PC_Building_Application.Data.Models;
+using PC_Building_Application.Data.Models.Dtos.RAM_Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Profiles
+{
+    public class RAMProfiles : Profile
+    {
+        public RAMProfiles()
+        {
+            CreateMap<RAM, RAMReadDto>();
+            CreateMap<RAM, RAMPatchDto>();
+            CreateMap<RAMPatchDto, RAM>();
+            CreateMap<RAMCreateDto, RAM>();
+        }
+    }
+}
