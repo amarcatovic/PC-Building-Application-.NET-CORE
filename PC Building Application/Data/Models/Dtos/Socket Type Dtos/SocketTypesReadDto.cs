36ce6b9ab694163d3c18926a306c37d240800fbd
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 11:57:02 +0200

    Add classic read dto

---
+++ PC Building Application/Data/Models/Dtos/Socket Type Dtos/SocketTypesReadDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Socket_Type_Dtos
+{
+    public class SocketTypesReadDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
