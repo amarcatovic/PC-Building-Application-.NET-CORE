6ceac45a7355da261ffc3a0d249732c232dd6f48
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 17:48:02 +0200

    Add GPU model

---
+++ PC Building Application/Data/Models/GPU.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models
+{
+    public class GPU
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Released { get; set; }
+        public string PCIPort { get; set; }
+        public string MemoryType { get; set; }
+        public string VRAM { get; set; }
+        public int NoOfHDMIPorts { get; set; }
+        public int NoOfDisplayPorts { get; set; }
+        public bool HasVGA { get; set; }
+        public bool HasDVI { get; set; }
+        public float Price { get; set; }
+        public Photo Photo { get; set; }
+        public int PhotoId { get; set; }
+        public Manufacturer ManufacturerId { get; set; }
+        public ICollection<PC> PCs { get; set; }
+    }
+}
