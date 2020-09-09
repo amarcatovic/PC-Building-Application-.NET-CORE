c5bab9587330b8eb8c1f6a14512bf43715e33348
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 18:11:25 +0200

    add gpu and cooler

--- PC Building Application/Data/Models/Manufacturer.cs
+++ PC Building Application/Data/Models/Manufacturer.cs
@@ -13,6 +13,8 @@ namespace PC_Building_Application.Data.Models
         public string City { get; set; }
 
         public ICollection<CPU> CPUs { get; set; }
+        public ICollection<GPU> GPUs { get; set; }
+        public ICollection<Cooler> Coolers { get; set; }
         public ICollection<Motherboard> Motherboards { get; set; }
         public ICollection<RAM> RAMs { get; set; }
     }
