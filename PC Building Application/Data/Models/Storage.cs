bfa2bcb74844dbb4708efbc28ef5c44c05b4d6d4
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 19:13:48 +0200

    add storage model

---
+++ PC Building Application/Data/Models/Storage.cs
@@ -0,0 +1,24 @@
+﻿using PC_Building_Application.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data
+{
+    public class Storage
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Released { get; set; }
+        public string Capacity { get; set; }
+        public StorageType StorageType { get; set; }
+        public int StorageTypeId { get; set; }
+        public bool HasCooling { get; set; }
+        public Photo Photo { get; set; }
+        public int PhotoId { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+        public int ManufacturerId { get; set; }
+        public ICollection<PCStorage> PCStorages { get; set; }
+    }
+}
