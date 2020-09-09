a074db4527bd9167e090f6300d570a9eeadb2053
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 19:21:48 +0200

    add migration to update cooler

---
+++ PC Building Application/Migrations/20200909172132_AddNumberOfFansToCooler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PC_Building_Application.Migrations
+{
+    public partial class AddNumberOfFansToCooler : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "NoOfFans",
+                table: "Coolers",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NoOfFans",
+                table: "Coolers");
+        }
+    }
+}
