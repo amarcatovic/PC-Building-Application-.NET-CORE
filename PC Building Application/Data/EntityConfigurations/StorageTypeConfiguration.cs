903c410569d1d54dbded6519fa548cf40c9e9377
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 19:14:34 +0200

    add storage type entity config

---
+++ PC Building Application/Data/EntityConfigurations/StorageTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.EntityConfigurations
+{
+    public class StorageTypeConfiguration : IEntityTypeConfiguration<StorageType>
+    {
+        public void Configure(EntityTypeBuilder<StorageType> builder)
+        {
+            builder.HasMany(st => st.Storages)
+                .WithOne(s => s.StorageType)
+                .HasForeignKey(s => s.StorageTypeId);
+        }
+    }
+}
