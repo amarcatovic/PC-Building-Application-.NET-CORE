8fd5e07880e333e53771c81b52ac30d99893c989
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 17:01:17 +0200

    Add migrations for RAM and Join table

---
+++ PC Building Application/Migrations/20200909150103_AddRAMAndJoinTableWithPC.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PC_Building_Application.Migrations
+{
+    public partial class AddRAMAndJoinTableWithPC : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RAMs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Speed = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NoOfSticks = table.Column<short>(type: "smallint", nullable: false),
+                    CapacityPerStick = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HasRGB = table.Column<bool>(type: "bit", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    PhotoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RAMs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RAMs_Photos_PhotoId",
+                        column: x => x.PhotoId,
+                        principalTable: "Photos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PCRAM",
+                columns: table => new
+                {
+                    PCId = table.Column<int>(type: "int", nullable: false),
+                    RAMId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PCRAM", x => new { x.PCId, x.RAMId });
+                    table.ForeignKey(
+                        name: "FK_PCRAM_PCs_PCId",
+                        column: x => x.PCId,
+                        principalTable: "PCs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PCRAM_RAMs_RAMId",
+                        column: x => x.RAMId,
+                        principalTable: "RAMs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PCRAM_RAMId",
+                table: "PCRAM",
+                column: "RAMId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RAMs_PhotoId",
+                table: "RAMs",
+                column: "PhotoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PCRAM");
+
+            migrationBuilder.DropTable(
+                name: "RAMs");
+        }
+    }
+}
