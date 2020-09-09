a399cdd66f3dcaa4ec10cc0965540e98fffef700
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 13:33:14 +0200

    Add interface for repo

---
+++ PC Building Application/Data/Repositories/Interfaces/ICPURepo.cs
@@ -0,0 +1,17 @@
+﻿using PC_Building_Application.Data.Models;
+using PC_Building_Application.Data.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories.Interfaces
+{
+    public interface ICPURepo
+    {
+        Task<ICollection<CPU>> GetCPUs();
+        Task<CPU> GetCPUById(int id);
+        Task CreateCPU(CPU cpu, PhotoToCreateDto photo);
+        Task<bool> Done();
+    }
+}
