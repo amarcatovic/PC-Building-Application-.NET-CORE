3c2c9fcc89ed42ba05c534e90245ae57afe40bdf
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 18:10:31 +0200

    Add Cooler Model

---
+++ PC Building Application/Data/Models/Cooler.cs
@@ -0,0 +1,25 @@
+﻿using PC_Building_Application.Data.Models.Join_Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models
+{
+    public class Cooler
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Released { get; set; }
+        public bool IsWaterCooler { get; set; }
+        public bool HasRGB { get; set; }
+        public float Price { get; set; }
+        public Photo Photo { get; set; }
+        public int PhotoId { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+        public int ManufacturerId { get; set; }
+        public ICollection<CoolerSocketType> CoolerSocketTypes { get; set; }
+        public ICollection<PC> PCs { get; set; }
+
+    }
+}
