841c5fc253e728a1041404fb55093284e069ff96
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 17:04:29 +0200

    Add RAM interface for repo

---
+++ PC Building Application/Data/Repositories/Interfaces/IRAMRepo.cs
@@ -0,0 +1,17 @@
+﻿using PC_Building_Application.Data.Models;
+using PC_Building_Application.Data.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories.Interfaces
+{
+    interface IRAMRepo
+    {
+        Task<IEnumerable<RAM>> GetAllRAMs();
+        Task<RAM> GetRAMById(int id);
+        Task CreateRAM(RAM ram, PhotoToCreateDto photo);
+        Task<int> Done();
+    }
+}
