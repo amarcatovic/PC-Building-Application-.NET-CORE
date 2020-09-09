8cd22875d55f788a0d9d1fd6d7b269f6006d4388
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 14:22:43 +0200

    Create motherboard Patch dto

---
+++ PC Building Application/Data/Models/Dtos/Motherboard Dtos/MotherboardPatchDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Motherboard_Dtos
+{
+    public class MotherboardPatchDto : MotherboardCreateDto
+    {
+    }
+}
