b412613ce0d155002d680cd9b37f5c0dbd1175b8
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 19:13:18 +0200

    Add join class

---
+++ PC Building Application/Data/Models/Join Classes/PCStorage.cs
@@ -0,0 +1,16 @@
+﻿using PC_Building_Application.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data
+{
+    public class PCStorage
+    {
+        public PC PC { get; set; }
+        public int PCId { get; set; }
+        public Storage Storage { get; set; }
+        public int StorageId { get; set; }
+    }
+}
