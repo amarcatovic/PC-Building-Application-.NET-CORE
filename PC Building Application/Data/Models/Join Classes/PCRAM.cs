68fce1dcec163f00b47602332620ae982ea752b8
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 15:33:00 +0200

    Add JoinClass between PC and RAM

---
+++ PC Building Application/Data/Models/Join Classes/PCRAM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Join_Classes
+{
+    public class PCRAM
+    {
+        public PC PC { get; set; }
+        public int PCId { get; set; }
+        public RAM RAM { get; set; }
+        public int RAMId { get; set; }
+    }
+}
