7935dd6699b34cfaadd0340a111f74105d4654b7
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 15:39:47 +0200

    Add RAM Entity Configuration

---
+++ PC Building Application/Data/EntityConfigurations/RAMConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PC_Building_Application.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.EntityConfigurations
+{
+    public class RAMConfiguration : IEntityTypeConfiguration<RAM>
+    {
+        public void Configure(EntityTypeBuilder<RAM> builder)
+        {
+            builder.HasKey(ram => ram.Id);
+
+            builder.Property(ram => ram.Price)
+                .IsRequired();
+
+            builder.Property(ram => ram.Name)
+                .IsRequired();
+
+            builder.Property(ram => ram.NoOfSticks)
+                .IsRequired();
+
+            builder.Property(ram => ram.Speed)
+                .IsRequired();
+
+            builder.Property(ram => ram.CapacityPerStick)
+                .IsRequired();
+
+            builder.Property(ram => ram.HasRGB)
+                .IsRequired();
+
+            builder.Property(ram => ram.Type)
+                .IsRequired();
+        }
+    }
+}
