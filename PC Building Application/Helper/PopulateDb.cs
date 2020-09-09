2c006551a15a7cadfb311de4d9edd49fce28e925
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 14:55:30 +0200

    Add class to populate DB at first run

---
+++ PC Building Application/Helper/PopulateDb.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PC_Building_Application.Data;
+using PC_Building_Application.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Helper
+{
+    public static class PopulateDb
+    {
+        public static void Populate(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<DataContext>());
+            }
+        }
+
+        public static void SeedData(DataContext context)
+        {
+            Console.WriteLine("Running SeedData Method...");
+            context.Database.Migrate();
+
+            if (!context.Photos.Any())
+            {
+                Console.WriteLine("Adding Picture for Annonymous Users to the database!");
+                context.Photos.AddRange
+                (
+                    new Photo() 
+                        { 
+                            Url = "https://i1.sndcdn.com/avatars-000437232558-yuo0mv-t500x500.jpg",
+                            Description = "Anonymous User Photo",
+                            DateAdded = DateTime.Now,
+                            PublicId = "0"
+                        }                               
+                );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("There are some photos in the database!");
+            }
+
+            if(!context.SocketTypes.Any())
+            {
+                Console.WriteLine("Adding the most popular socket types to the database!");
+                context.SocketTypes.AddRange
+                (
+                    new SocketType() {Name = "AM4"},
+                    new SocketType() { Name = "TR4" },
+                    new SocketType() { Name = "sTRX4" },
+                    new SocketType() { Name = "LGA1150" },
+                    new SocketType() { Name = "LGA1151" },
+                    new SocketType() { Name = "LGA1155" },
+                    new SocketType() { Name = "LGA2066" }
+                );
+            }
+            else
+            {
+                Console.WriteLine("There are some Socket Types in the database!");
+            }
+
+            if(!context.Manufacturers.Any())
+            {
+                Console.WriteLine("Adding the most popular Manufacturers to the database!");
+                context.AddRange
+                (
+                    new Manufacturer() { Name = "AMD", City = "Santa Clara, CA", Country = "U.S.A"},
+                    new Manufacturer() { Name = "Intel", City = "Santa Clara, CA", Country = "U.S.A" },
+                    new Manufacturer() { Name = "Asus", City = "Taipei", Country = "Taiwan" },
+                    new Manufacturer() { Name = "MSI", City = "New Taipei", Country = "Taiwan" },
+                    new Manufacturer() { Name = "Nvidia", City = "Santa Clara, CA", Country = "U.S.A" },
+                    new Manufacturer() { Name = "NZXT", City = "City of Industry, CA", Country = "U.S.A" },
+                    new Manufacturer() { Name = "EVGA Corporation", City = "Santa Clara, CA", Country = "U.S.A" },
+                    new Manufacturer() { Name = "Samsung", City = "Seoul", Country = "South Korea" },
+                    new Manufacturer() { Name = "Kingston Technology", City = "Fountain Valley, CA", Country = "U.S.A" },
+                    new Manufacturer() { Name = "Micron Technology", City = "Boise, ID", Country = "U.S.A" },
+                    new Manufacturer() { Name = "Cooler Master", City = "New Taipei", Country = "Taiwan" }
+                );
+            }
+            else
+            {
+                Console.WriteLine("There are some Manufacturers in the database!");
+            }
+        }
+    }
+}
