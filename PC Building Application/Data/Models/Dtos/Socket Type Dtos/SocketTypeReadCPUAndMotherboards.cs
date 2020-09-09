94204fbd0e33bd94f6230b7e9877b657ff30ab2b
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 11:35:52 +0200

    With Motherboards and cpus dto

---
+++ PC Building Application/Data/Models/Dtos/Socket Type Dtos/SocketTypeReadCPUAndMotherboards.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Socket_Type_Dtos
+{
+    public class SocketTypeReadCPUAndMotherboards
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<CPU> CPUs { get; set; }
+        public ICollection<Motherboard> Motherboards { get; set; }
+    }
+}
