36e8801c765af85f908f474157752b06d46cb714
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 19:13:42 +0200

    add storage type model

---
+++ PC Building Application/Data/Models/StorageType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data
+{
+    public class StorageType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Storage> Storages { get; set; }
+    }
+}
