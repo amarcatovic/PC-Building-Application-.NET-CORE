f16dca1b3eff1f32b7770a6ca5278359c4153c71
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 11:42:29 +0200

    Create mapping profile for Socket Types

---
+++ PC Building Application/Profiles/SocketTypeProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using PC_Building_Application.Data.Models;
+using PC_Building_Application.Data.Models.Dtos.Socket_Type_Dtos;
+
+namespace PC_Building_Application.Profiles
+{
+    public class SocketTypeProfiles : Profile
+    {
+        public SocketTypeProfiles()
+        {
+            CreateMap<SocketTypeCreateDto, SocketType>();
+            CreateMap<SocketType, SocketTypeReadCPUAndMotherboards>();
+            CreateMap<SocketType, SocketTypeReadCPUDto>();
+            CreateMap<SocketType, SocketTypeReadMotherboardsDto>();
+        }
+    }
+}
