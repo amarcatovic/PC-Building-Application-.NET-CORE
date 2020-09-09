4b30a73e1d7af40f72d51cdf5127f65269cd8ec3
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 13:43:15 +0200

    Dto For creating a new CPU

---
+++ PC Building Application/Data/Models/Dtos/CPU Dtos/CPUCreateDto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.CPU_Dtos
+{
+    public class CPUCreateDto
+    {
+        public string Name { get; set; }
+        public DateTime Released { get; set; }
+        public int SocketTypeId { get; set; }
+        public string Clockspeed { get; set; }
+        public string TurboSpeed { get; set; }
+        public int NoOfCores { get; set; }
+        public int SingleThreadRating { get; set; }
+        public int ManufacturerId { get; set; }
+        public int PhotoId { get; set; }
+        public string PhotoDescription { get; set; }
+        public IFormFile PhotoFile { get; set; }
+    }
+}
