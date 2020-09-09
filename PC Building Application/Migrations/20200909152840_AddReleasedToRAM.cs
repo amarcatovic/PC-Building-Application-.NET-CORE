8fa4b4242080d51931895058218575be8b66b1d6
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-09 17:29:21 +0200

    Add Date Released to RAM

---
+++ PC Building Application/Migrations/20200909152840_AddReleasedToRAM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PC_Building_Application.Migrations
+{
+    public partial class AddReleasedToRAM : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Released",
+                table: "RAMs",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Released",
+                table: "RAMs");
+        }
+    }
+}
