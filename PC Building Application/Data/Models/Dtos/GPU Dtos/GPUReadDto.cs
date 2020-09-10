5e67ffced3eb9d56e4e22ec3d368a9c45bc78472
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 12:26:11 +0200

    create GPU Read Dto

---
+++ PC Building Application/Data/Models/Dtos/GPU Dtos/GPUReadDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.GPU_Dtos
+{
+    public class GPUReadDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Released { get; set; }
+        public string PCIPort { get; set; }
+        public string MemoryType { get; set; }
+        public string VRAM { get; set; }
+        public int NoOfHDMIPorts { get; set; }
+        public int NoOfDisplayPorts { get; set; }
+        public bool HasVGA { get; set; }
+        public bool HasDVI { get; set; }
+        public float Price { get; set; }
+        public PhotoReturnDto Photo { get; set; }
+        public string Manufacturer { get; set; }
+    }
+}
