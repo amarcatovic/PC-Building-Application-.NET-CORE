25315507f85951bd479b2c17e53bf85e302e4900
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 22:28:05 +0200

    Add dto to read all gpus

---
+++ PC Building Application/Data/Models/Dtos/Manufacturer Dtos/ManufacturerReadAllGpusDto.cs
@@ -0,0 +1,18 @@
+﻿using PC_Building_Application.Data.Models.Dtos.GPU_Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Manufacturer_Dtos
+{
+    public class ManufacturerReadAllGpusDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+
+        public ICollection<GPUReadDto> GPUs { get; set; }
+    }
+}
