2de071ac04236e22c21608fa79d887d9d546ee48
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 17:03:09 +0200

    Add join table repo interface

---
+++ PC Building Application/Data/Repositories/Interfaces/ICoolerSocketTypeRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories.Interfaces
+{
+    public interface ICoolerSocketTypeRepo
+    {
+        Task<bool> InsertCoolerWithSocketTypes(int coolerId, IEnumerable<int> socketTypes);
+    }
+}
