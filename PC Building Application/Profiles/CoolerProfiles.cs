186cc81c5b39847112cc4a792e0bf9fd328ad9c1
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 14:57:25 +0200

    Add cooler profiles

---
+++ PC Building Application/Profiles/CoolerProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using PC_Building_Application.Data.Models;
+using PC_Building_Application.Data.Models.Dtos.Cooler_Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Profiles
+{
+    public class CoolerProfiles : Profile
+    {
+        public CoolerProfiles()
+        {
+            CreateMap<Cooler, CoolerReadDto>()
+                .ForMember(dest => dest.Manufacturer, opt => opt.MapFrom(src => src.Manufacturer.Name));
+
+            CreateMap<Cooler, CoolerPatchDto>().ReverseMap();
+            CreateMap<CoolerCreateDto, Cooler>();
+        }
+    }
+}
