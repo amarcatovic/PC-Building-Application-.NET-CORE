9e6b0b7ad673afd511004eeac90e74963ec6be5b
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 22:34:58 +0200

    Add read all manufacturer storages dto

---
+++ PC Building Application/Data/Models/Dtos/Manufacturer Dtos/ManufacturerReadAllStoragesDto.cs
@@ -0,0 +1,18 @@
+﻿using PC_Building_Application.Data.Models.Dtos.Storage_Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Manufacturer_Dtos
+{
+    public class ManufacturerReadAllStoragesDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public PhotoReturnDto Photo { get; set; }
+        public ICollection<StorageReadDto> Storage { get; set; }
+    }
+}
