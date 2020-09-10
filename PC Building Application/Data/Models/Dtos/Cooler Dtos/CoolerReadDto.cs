a6a27b4b71e4e02c9ca89024ad1e97dca11a8bbf
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 14:43:50 +0200

    add cooler read dto

---
+++ PC Building Application/Data/Models/Dtos/Cooler Dtos/CoolerReadDto.cs
@@ -0,0 +1,22 @@
+﻿using PC_Building_Application.Data.Models.Dtos.Socket_Type_Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Cooler_Dtos
+{
+    public class CoolerReadDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Released { get; set; }
+        public bool IsWaterCooler { get; set; }
+        public byte NoOfFans { get; set; }
+        public bool HasRGB { get; set; }
+        public float Price { get; set; }
+        public PhotoReturnDto Photo { get; set; }
+        public string Manufacturer { get; set; }
+        public ICollection<SocketTypesReadDto> Sockets { get; set; }
+    }
+}
