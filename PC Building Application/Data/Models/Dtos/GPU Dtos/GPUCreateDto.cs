2891b212eba34c490e50319fa571ee9fcd6d91a2
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 12:24:32 +0200

    Add GPU Create dto

---
+++ PC Building Application/Data/Models/Dtos/GPU Dtos/GPUCreateDto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.GPU_Dtos
+{
+    public class GPUCreateDto
+    {
+        public string Name { get; set; }
+        public DateTime Released { get; set; }
+        public string PCIPort { get; set; }
+        public string MemoryType { get; set; }
+        public string VRAM { get; set; }
+        public int NoOfHDMIPorts { get; set; }
+        public int NoOfDisplayPorts { get; set; }
+        public bool HasVGA { get; set; }
+        public bool HasDVI { get; set; }
+        public float Price { get; set; }
+        public int PhotoId { get; set; }
+        public int ManufacturerId { get; set; }
+        public string PhotoDescription { get; set; }
+        public IFormFile PhotoFile { get; set; }
+    }
+}
