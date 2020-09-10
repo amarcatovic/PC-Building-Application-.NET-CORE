0a841f599859f69f90299db2e65945fe3fbf114f
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 14:46:30 +0200

    add cooler patch dto

---
+++ PC Building Application/Data/Models/Dtos/Cooler Dtos/CoolerPatchDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Cooler_Dtos
+{
+    public class CoolerPatchDto : CoolerCreateDto
+    {
+    }
+}
