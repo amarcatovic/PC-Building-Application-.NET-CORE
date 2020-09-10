40139191b33ef8003835b74a6643250b99bd6148
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 18:03:03 +0200

    add manufacturer repo

---
+++ PC Building Application/Data/Repositories/ManufacturerRepo.cs
@@ -0,0 +1,87 @@
+﻿using PC_Building_Application.Data.Models;
+using PC_Building_Application.Data.Models.Dtos;
+using PC_Building_Application.Data.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories
+{
+    public class ManufacturerRepo : IManufacturerRepo
+    {
+        private readonly DataContext _context;
+        private readonly IPhotoRepo _photoRepo;
+        public ManufacturerRepo(DataContext context, IPhotoRepo photoRepo)
+        {
+            _context = context;
+            _photoRepo = photoRepo;
+        }
+
+        public async Task CraeteManufacturer(Manufacturer manufacturer, PhotoToCreateDto photo)
+        {
+            /*var createdPhoto = await _photoRepo.AddPhotoForComponent(photo);
+            if (createdPhoto == null)
+                return;
+
+            manufacturer.PhotoId = createdPhoto.Id;
+            await _context.Cases.AddAsync(manufacturer);*/
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Done()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Manufacturer>> GetAllManufacturers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Manufacturer> GetManufacturerById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Manufacturer> GetManufacturerCasesById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Manufacturer> GetManufacturerCoolersById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Manufacturer> GetManufacturerCpusById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Manufacturer> GetManufacturerGpusById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Manufacturer> GetManufacturerMotherboardsById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Manufacturer> GetManufacturerPowerSuppliesById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Manufacturer> GetManufacturerRamsById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Manufacturer> GetManufacturerStoragesById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
