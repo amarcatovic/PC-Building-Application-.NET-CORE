aee0a87cd11baa4e24d217e9145700eb476d81f6
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 13:34:33 +0200

    Add Case Profiles

---
+++ PC Building Application/Profiles/CaseProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using PC_Building_Application.Data.Models;
+using PC_Building_Application.Data.Models.Dtos.Case_Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Profiles
+{
+    public class CaseProfiles : Profile
+    {
+        public CaseProfiles()
+        {
+            CreateMap<Case, CaseReadDto>()
+                .ForMember(dest => dest.Manufacturer, opt => opt.MapFrom(src => src.Manufacturer.Name));
+
+            CreateMap<Case, CasePatchDto>().ReverseMap();
+            CreateMap<CaseCreateDto, Case>();
+        }
+    }
+}
