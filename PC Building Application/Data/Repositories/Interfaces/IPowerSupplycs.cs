3903d1d5a8682f794103857661f07c5df3873332
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 12:45:34 +0200

    Add PSU repo interface

---
+++ PC Building Application/Data/Repositories/Interfaces/IPowerSupplycs.cs
@@ -0,0 +1,17 @@
+﻿using PC_Building_Application.Data.Models;
+using PC_Building_Application.Data.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories.Interfaces
+{
+    public interface IPowerSupplycs
+    {
+        Task<IEnumerable<PowerSupply>> GetAllPowerSupplys();
+        Task<PowerSupply> GetPowerSupplyById(int id);
+        Task CreatePowerSupply(PowerSupply powerSupply, PhotoToCreateDto photo);
+        Task<int> Done();
+    }
+}
