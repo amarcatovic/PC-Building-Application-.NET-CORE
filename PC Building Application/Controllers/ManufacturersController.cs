3ddccf4fe27772e446c86d8f6f9251011a139b7c
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 22:06:14 +0200

    Add GET  all method

---
+++ PC Building Application/Controllers/ManufacturersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PC_Building_Application.Data.Models.Dtos.Manufacturer_Dtos;
+using PC_Building_Application.Data.Repositories.Interfaces;
+
+namespace PC_Building_Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManufacturersController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private IManufacturerRepo _repo;
+
+        public ManufacturersController(IMapper mapper, IManufacturerRepo repo)
+        {
+            _mapper = mapper;
+            _repo = repo;
+        }
+
+        [HttpGet("products/all")]
+        public async Task<IActionResult> GetManufacturersWithAllProducts()
+        {
+            var manufacturersFromDb = await _repo.GetAllManufacturers();
+            var manufacturerReadAllDto = _mapper.Map<IEnumerable<ManufacturerReadAllDto>>(manufacturersFromDb);
+
+            return Ok(manufacturerReadAllDto);
+        }
+    }
+}
