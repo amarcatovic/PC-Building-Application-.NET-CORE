ce5eaad75fd33de17253c54917120aeea9cd2033
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 14:03:42 +0200

    Add storage read dto

---
+++ PC Building Application/Data/Models/Dtos/Storage Dtos/StorageReadDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Storage_Dtos
+{
+    public class StorageReadDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Released { get; set; }
+        public string Capacity { get; set; }
+        public string StorageType { get; set; }
+        public bool HasCooling { get; set; }
+        public PhotoReturnDto Photo { get; set; }
+        public string Manufacturer { get; set; }
+    }
+}
