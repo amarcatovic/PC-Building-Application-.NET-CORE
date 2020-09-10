d795b8f2470cdc806b59a1f0e3d1abc22a6c2c14
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 18:03:13 +0200

    add migrations for adding photo to manufacturer

---
+++ PC Building Application/Migrations/20200910160200_AddPhotoIdToManufacturer.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PC_Building_Application.Migrations
+{
+    public partial class AddPhotoIdToManufacturer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PhotoId",
+                table: "Manufacturers",
+                type: "int",
+                nullable: false,
+                defaultValue: 1);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Manufacturers_PhotoId",
+                table: "Manufacturers",
+                column: "PhotoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Manufacturers_Photos_PhotoId",
+                table: "Manufacturers",
+                column: "PhotoId",
+                principalTable: "Photos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Manufacturers_Photos_PhotoId",
+                table: "Manufacturers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Manufacturers_PhotoId",
+                table: "Manufacturers");
+
+            migrationBuilder.DropColumn(
+                name: "PhotoId",
+                table: "Manufacturers");
+        }
+    }
+}
