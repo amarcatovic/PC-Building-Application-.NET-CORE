6431baf1e19908416ae48247c00985540f0a771b
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 14:06:01 +0200

    Add storage profiles

---
+++ PC Building Application/Profiles/StorageProfiles.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using PC_Building_Application.Data;
+using PC_Building_Application.Data.Models.Dtos.Storage_Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Profiles
+{
+    public class StorageProfiles : Profile
+    {
+        public StorageProfiles()
+        {
+            CreateMap<Storage, StorageReadDto>()
+                .ForMember(dest => dest.StorageType, opt => opt.MapFrom(src => src.StorageType.Name))
+                .ForMember(dest => dest.Manufacturer, opt => opt.MapFrom(src => src.Manufacturer.Name));
+
+            CreateMap<Storage, StoragePatchDto>().ReverseMap();
+            CreateMap<StorageCreateDto, Storage>();
+        }
+    }
+}
