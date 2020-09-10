aa069240a0ba4b2b8da4f728a8e29c81a34e3fe5
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 13:55:55 +0200

    create storage repo interface

---
+++ PC Building Application/Data/Repositories/Interfaces/IStorageRepo.cs
@@ -0,0 +1,16 @@
+﻿using PC_Building_Application.Data.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories.Interfaces
+{
+    public interface IStorageRepo
+    {
+        Task<IEnumerable<Storage>> GetStorages();
+        Task<Storage> GetStorageById(int id);
+        Task CreateStorage(Storage storage, PhotoToCreateDto photo);
+        Task<bool> Done();
+    }
+}
