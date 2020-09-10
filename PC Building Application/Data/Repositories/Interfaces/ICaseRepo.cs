ac7ba347fe54c206e54426d4717b726e68f38baa
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 13:24:52 +0200

    Create case repo interface

---
+++ PC Building Application/Data/Repositories/Interfaces/ICaseRepo.cs
@@ -0,0 +1,17 @@
+﻿using PC_Building_Application.Data.Models;
+using PC_Building_Application.Data.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories.Interfaces
+{
+    public interface ICaseRepo
+    {
+        Task<IEnumerable<Case>> GetCases();
+        Task<Case> GetCaseById(int id);
+        Task CreateCase(Case @case, PhotoToCreateDto photo);
+        Task<bool> Done();
+    }
+}
