a66d17cc4e56ab864b92bd6f8edf5074f22452c8
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 17:53:10 +0200

    Add manufacturer repo interface

---
+++ PC Building Application/Data/Repositories/Interfaces/IManufacturerRepo.cs
@@ -0,0 +1,22 @@
+﻿using PC_Building_Application.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories.Interfaces
+{
+    public interface IManufacturerRepo
+    {
+        Task<IEnumerable<Manufacturer>> GetAllManufacturers();
+        Task<Manufacturer> GetManufacturerById(int id);
+        Task<Manufacturer> GetManufacturerCpusById(int id);
+        Task<Manufacturer> GetManufacturerGpusById(int id);
+        Task<Manufacturer> GetManufacturerCoolersById(int id);
+        Task<Manufacturer> GetManufacturerMotherboardsById(int id);
+        Task<Manufacturer> GetManufacturerRamsById(int id);
+        Task<Manufacturer> GetManufacturerPowerSuppliesById(int id);
+        Task<Manufacturer> GetManufacturerCasesById(int id);
+        Task<Manufacturer> GetManufacturerStoragesById(int id);
+    }
+}
