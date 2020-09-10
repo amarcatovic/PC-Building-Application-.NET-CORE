c7c9c0f34a0dcead76cb6fd2feedf9888876b415
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 14:37:16 +0200

    Add cooler repo interface

---
+++ PC Building Application/Data/Repositories/Interfaces/ICoolerRepo.cs
@@ -0,0 +1,17 @@
+﻿using PC_Building_Application.Data.Models;
+using PC_Building_Application.Data.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories.Interfaces
+{
+    public interface ICoolerRepo
+    {
+        Task<IEnumerable<Cooler>> GetCoolers();
+        Task<Cooler> GetCoolerById(int id);
+        Task CreateCooler(Cooler cooler, PhotoToCreateDto photo);
+        Task<bool> Done();
+    }
+}
