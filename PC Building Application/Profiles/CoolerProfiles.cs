9793b9a6fda6f3ee910153b852768a03cd37bcb4
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 16:12:53 +0200

    Custom map for cooler read dto

--- PC Building Application/Profiles/CoolerProfiles.cs
+++ PC Building Application/Profiles/CoolerProfiles.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using PC_Building_Application.Data.Models;
 using PC_Building_Application.Data.Models.Dtos.Cooler_Dtos;
+using PC_Building_Application.Data.Models.Dtos.Socket_Type_Dtos;
+using PC_Building_Application.Data.Models.Join_Classes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,10 @@ namespace PC_Building_Application.Profiles
         public CoolerProfiles()
         {
             CreateMap<Cooler, CoolerReadDto>()
-                .ForMember(dest => dest.Manufacturer, opt => opt.MapFrom(src => src.Manufacturer.Name));
+                .ForMember(dest => dest.Manufacturer, opt => opt.MapFrom(src => src.Manufacturer.Name))
+                .ForMember(dest => dest.Sockets, opt => opt.MapFrom(src => src.CoolerSocketTypes
+                                                                .Select(st => st.SocketType)
+                                                                .Select(st => st.Name)));
 
             CreateMap<Cooler, CoolerPatchDto>().ReverseMap();
             CreateMap<CoolerCreateDto, Cooler>();
