8f7d27d034b94fd9010dadbaa69847f23d0de012
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 21:54:08 +0200

    Add manufacturer create dto

---
+++ PC Building Application/Data/Models/Dtos/Manufacturer Dtos/ManufacturerCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Manufacturer_Dtos
+{
+    public class ManufacturerCreateDto
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string PhotoDescription { get; set; }
+        public IFormFile PhotoFile { get; set; }
+    }
+}
