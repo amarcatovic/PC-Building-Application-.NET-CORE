7f3a428844abc6bcf94d8ab46f46bb6381ad537f
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 13:01:01 +0200

    Add PSU profiles

---
+++ PC Building Application/Profiles/PowerSupplyProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using PC_Building_Application.Data.Models;
+using PC_Building_Application.Data.Models.Dtos.Power_Supply_Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Profiles
+{
+    public class PowerSupplyProfiles : Profile
+    {
+        public PowerSupplyProfiles()
+        {
+            CreateMap<PowerSupply, PowerSupplyReadDto>()
+                .ForMember(dest => dest.Manufacturer, opt => opt.MapFrom(src => src.Manufacturer.Name));
+            
+            CreateMap<PowerSupply, PowerSupplyPatchDto>().ReverseMap();
+            CreateMap<PowerSupplyCreateDto, PowerSupply>();
+        }
+    }
+}
