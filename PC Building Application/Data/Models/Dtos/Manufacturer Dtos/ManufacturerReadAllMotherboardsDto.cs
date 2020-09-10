3ca73f5a9dca6b7f12cc607bc6365f0c7fa051be
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 22:31:05 +0200

    Add read all mobos dto

---
+++ PC Building Application/Data/Models/Dtos/Manufacturer Dtos/ManufacturerReadAllMotherboardsDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Manufacturer_Dtos
+{
+    public class ManufacturerReadAllMotherboardsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+
+        public ICollection<MotherboardReadDto> Motherboards { get; set; }
+    }
+}
