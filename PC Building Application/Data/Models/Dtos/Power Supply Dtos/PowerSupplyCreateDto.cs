06e96dbb1e1f6eacf4ae0097ef7533b5378b53da
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 12:54:05 +0200

    create psu create dto

---
+++ PC Building Application/Data/Models/Dtos/Power Supply Dtos/PowerSupplyCreateDto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Power_Supply_Dtos
+{
+    public class PowerSupplyCreateDto
+    {
+        public string Name { get; set; }
+        public DateTime Released { get; set; }
+        public string Power { get; set; }
+        public byte NoOfPCIe6Pins { get; set; }
+        public byte NoOfPCIe8Pins { get; set; }
+        public byte NoOfSATACables { get; set; }
+        public byte NoOfCPUCables { get; set; }
+        public bool Has24PinCable { get; set; }
+        public byte EfficiencyRating { get; set; }
+        public float Price { get; set; }
+        public int PhotoId { get; set; }
+        public int ManufacturerId { get; set; }
+        public string PhotoDescription { get; set; }
+        public IFormFile PhotoFile { get; set; }
+    }
+}
