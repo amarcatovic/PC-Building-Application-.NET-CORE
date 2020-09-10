43f98a864f95832f93773369cda7d975b7328ff0
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 12:18:04 +0200

    Add GPU repo interface

---
+++ PC Building Application/Data/Repositories/Interfaces/IGPURepo.cs
@@ -0,0 +1,17 @@
+﻿using PC_Building_Application.Data.Models;
+using PC_Building_Application.Data.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories.Interfaces
+{
+    public interface IGPURepo
+    {
+        Task<IEnumerable<GPU>> GetAllGPUs();
+        Task<GPU> GetGPUById(int id);
+        Task CreateGPU(GPU gpu, PhotoToCreateDto photo);
+        Task<int> Done();
+    }
+}
