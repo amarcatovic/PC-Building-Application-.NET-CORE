9fcc43ed94892c084aa737793a42c30d6d562f90
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 13:15:28 +0200

    Add migrations for psu EfficiencyRating fix

---
+++ PC Building Application/Migrations/20200910111456_ChangeEfficiencyRatingToStringInPowerSupply.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PC_Building_Application.Migrations
+{
+    public partial class ChangeEfficiencyRatingToStringInPowerSupply : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EfficiencyRating",
+                table: "PowerSupplies",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "EfficiencyRating",
+                table: "PowerSupplies",
+                type: "tinyint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
