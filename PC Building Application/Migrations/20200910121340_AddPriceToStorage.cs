a69dba8002c3332fb5e439a53f56aa7af7dc1a65
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 14:13:55 +0200

    Add migration for price to storage

---
+++ PC Building Application/Migrations/20200910121340_AddPriceToStorage.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PC_Building_Application.Migrations
+{
+    public partial class AddPriceToStorage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Price",
+                table: "Storages",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Storages");
+        }
+    }
+}
