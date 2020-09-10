66c2b4866f4b3d874930a674dc4187803a7edfe8
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 12:28:07 +0200

    Create mapping profile for GPU

---
+++ PC Building Application/Profiles/GPUProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using PC_Building_Application.Data.Models;
+using PC_Building_Application.Data.Models.Dtos.GPU_Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Profiles
+{
+    public class GPUProfiles : Profile
+    {
+        public GPUProfiles()
+        {
+            CreateMap<GPU, GPUReadDto>()
+                .ForMember(dest => dest.Manufacturer, opt => opt.MapFrom(src => src.Manufacturer.Name));
+
+            CreateMap<GPU, GPUPatchDto>().ReverseMap();
+            CreateMap<GPUCreateDto, GPU>();
+        }
+    }
+}
