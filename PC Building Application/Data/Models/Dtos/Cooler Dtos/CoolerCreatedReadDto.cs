7c391f06041c3a2966706fc834cc14a5bcba9190
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 17:29:48 +0200

    Create dto for after new cooler is created

---
+++ PC Building Application/Data/Models/Dtos/Cooler Dtos/CoolerCreatedReadDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Cooler_Dtos
+{
+    public class CoolerCreatedReadDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
