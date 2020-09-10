1b067cc3478279b2e47cf37edafd45570234f091
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 13:30:01 +0200

    Create Case Dto for creation

---
+++ PC Building Application/Data/Models/Dtos/Case Dtos/CaseCreateDto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Case_Dtos
+{
+    public class CaseCreateDto
+    {
+        public string Name { get; set; }
+        public DateTime Released { get; set; }
+        public byte NoOfUSB3Ports { get; set; }
+        public bool HasRGB { get; set; }
+        public bool HasScreen { get; set; }
+        public string Size { get; set; }
+        public float Price { get; set; }
+        public int PhotoId { get; set; }
+        public int ManufacturerId { get; set; }
+        public string PhotoDescription { get; set; }
+        public IFormFile PhotoFile { get; set; }
+    }
+}
