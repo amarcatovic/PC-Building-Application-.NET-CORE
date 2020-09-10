0f78c109a3a286b8f9dea792b0bacb0985016f5c
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 23:46:56 +0200

    Create light read dto

---
+++ PC Building Application/Data/Models/Dtos/Manufacturer Dtos/ManufacturerReadDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Manufacturer_Dtos
+{
+    public class ManufacturerReadDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public PhotoReturnDto Photo { get; set; }
+    }
+}
