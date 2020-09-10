7cd21249958c5087709e11b91de457a8cb646b19
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 14:02:52 +0200

    Add storage patch dto

---
+++ PC Building Application/Data/Models/Dtos/Storage Dtos/StoragePatchDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Storage_Dtos
+{
+    public class StoragePatchDto : StorageCreateDto
+    {
+    }
+}
