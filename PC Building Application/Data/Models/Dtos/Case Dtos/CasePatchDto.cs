878d18bdb97d51de1325dcf5f11fbd8a6a8b82d6
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 13:30:31 +0200

    Add Patch dto

---
+++ PC Building Application/Data/Models/Dtos/Case Dtos/CasePatchDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Case_Dtos
+{
+    public class CasePatchDto : CaseCreateDto
+    {
+    }
+}
