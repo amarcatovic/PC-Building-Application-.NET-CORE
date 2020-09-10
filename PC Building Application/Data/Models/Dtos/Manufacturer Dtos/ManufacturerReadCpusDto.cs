3993847fcb809a419e0ad9adfa7a6500c9f223d2
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-10 22:26:58 +0200

    Dto to read all cpus of manufacturer

---
+++ PC Building Application/Data/Models/Dtos/Manufacturer Dtos/ManufacturerReadCpusDto.cs
@@ -0,0 +1,18 @@
+﻿using PC_Building_Application.Data.Models.Dtos.CPU_Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.Manufacturer_Dtos
+{
+    public class ManufacturerReadCpusDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+
+        public ICollection<CPUReadDto> CPUs { get; set; }
+    }
+}
