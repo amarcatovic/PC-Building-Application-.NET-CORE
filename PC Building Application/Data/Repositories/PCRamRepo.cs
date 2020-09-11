7a3ce451b998e234591b7a5263123a6f4ce3c7ec
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-11 17:52:05 +0200

    implement methods for adding and removing ram

--- PC Building Application/Data/Repositories/PCRamRepo.cs
+++ PC Building Application/Data/Repositories/PCRamRepo.cs
@@ -1,4 +1,5 @@
-﻿using PC_Building_Application.Data.Models.Join_Classes;
+﻿using Microsoft.EntityFrameworkCore;
+using PC_Building_Application.Data.Models.Join_Classes;
 using PC_Building_Application.Data.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,12 @@ namespace PC_Building_Application.Data.Repositories
         {
             _context = context;
         }
+
+        public async Task AddRam(int pcId, int ramId)
+        {
+            await _context.PCRAM.AddAsync(new PCRAM() { PCId = pcId, RAMId = ramId });
+        }
+
         public async Task<bool> InsertRAMIntoPC(int pcId, IEnumerable<int> ramIds)
         {
             foreach (var ramId in ramIds)
@@ -26,6 +33,14 @@ namespace PC_Building_Application.Data.Repositories
                 return true;
 
             return false;
+        }
+
+        public async Task RemoveRam(int pcId, int ramId)
+        {
+            var pcRamFromDb = await _context.PCRAM
+                .SingleOrDefaultAsync(pcs => pcs.RAMId == ramId && pcs.PCId == pcId);
+
+            _context.PCRAM.Remove(pcRamFromDb);
         }
     }
 }
