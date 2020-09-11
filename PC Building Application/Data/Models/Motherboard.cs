9f874ed532dfd69a3e54464ae011bd580ba0b306
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-11 19:05:51 +0200

    Add validations with other pc parts

--- PC Building Application/Data/Models/Motherboard.cs
+++ PC Building Application/Data/Models/Motherboard.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using CloudinaryDotNet.Actions;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace PC_Building_Application.Data.Models
@@ -21,9 +24,45 @@ namespace PC_Building_Application.Data.Models
         public float Price { get; set; }
         public Manufacturer Manufacturer { get; set; }
         public int ManufacturerId { get; set; }
-
         public Photo Photo { get; set; }
         public int PhotoId { get; set; }
         public ICollection<PC> PCs { get; set; }
+
+
+        // COMPATIBILITY CHECK METHODS
+        public List<string> CheckMotherboardAndCpuCompatibility(CPU cpu)
+        {
+            var errorStrings = new List<string>();
+            if (this.SocketType.Name != cpu.SocketType.Name)
+                errorStrings.Add($"CPU's socket {cpu.SocketType.Name} cannot fit onto motherboards socket {this.SocketType.Name}");
+
+            return errorStrings;
+        }
+
+        public List<string> CheckMotherboardAndRamsCompatibility(IEnumerable<RAM> rams)
+        {
+            var errorStrings = new List<string>();
+            int noOfRamSticks = 0;
+            bool compatibleMemoryType = true;
+            string ramName = "";
+
+            foreach(var ram in rams)
+            {
+                noOfRamSticks += ram.NoOfSticks;
+                if (ram.Type != this.MemoryType)
+                {
+                    compatibleMemoryType = false;
+                    ramName = ram.Name;
+                }                  
+            }
+
+            if (noOfRamSticks > this.NoOfRAMSlots)
+                errorStrings.Add($"Motherboard has {this.NoOfRAMSlots} slots for RAM, but there are {noOfRamSticks} included in PC build");
+
+            if (!compatibleMemoryType)
+                errorStrings.Add($"{ramName} and motherboard are incompatible. Please pick {this.MemoryType} RAM");
+
+            return errorStrings;
+        }
     }
 }
