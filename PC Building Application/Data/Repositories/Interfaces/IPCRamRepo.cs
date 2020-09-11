75a3b39137ab3575e1c9bc4f07a21043fb832622
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-11 15:27:34 +0200

    add interface for join class repo

---
+++ PC Building Application/Data/Repositories/Interfaces/IPCRamRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories.Interfaces
+{
+    public interface IPCRamRepo
+    {
+        Task<bool> InsertRAMIntoPC(int pcId, IEnumerable<int> ramIds);
+    }
+}
