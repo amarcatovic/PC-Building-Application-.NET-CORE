21c31abb231b853c1710910fbdf039b8730d60f6
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-11 13:48:41 +0200

    Add more default values

--- PC Building Application/Helper/PopulateDb.cs
+++ PC Building Application/Helper/PopulateDb.cs
@@ -31,12 +31,96 @@ namespace PC_Building_Application.Helper
                 context.Photos.AddRange
                 (
                     new Photo() 
-                        { 
-                            Url = "https://i1.sndcdn.com/avatars-000437232558-yuo0mv-t500x500.jpg",
-                            Description = "Anonymous User Photo",
-                            DateAdded = DateTime.Now,
-                            PublicId = "0"
-                        }                               
+                    { 
+                        Url = "https://i1.sndcdn.com/avatars-000437232558-yuo0mv-t500x500.jpg",
+                        Description = "Anonymous User Photo",
+                        DateAdded = DateTime.Now,
+                        PublicId = "1"
+                    },
+                    new Photo()
+                    {
+                        Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Desktop_computer_clipart_-_Yellow_theme.svg/1280px-Desktop_computer_clipart_-_Yellow_theme.svg.png",
+                        Description = "PC Photo",
+                        DateAdded = DateTime.Now,
+                        PublicId = "2"
+                    },
+                    new Photo()
+                    {
+                        Url = "https://upload.wikimedia.org/wikipedia/hr/thumb/7/7c/AMD_Logo.svg/1280px-AMD_Logo.svg.png",
+                        Description = "AMD Logo",
+                        DateAdded = DateTime.Now,
+                        PublicId = "3"
+                    },
+                    new Photo()
+                    {
+                        Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Intel_logo_%282020%2C_light_blue%29.svg/1280px-Intel_logo_%282020%2C_light_blue%29.svg.png",
+                        Description = "Intel Logo",
+                        DateAdded = DateTime.Now,
+                        PublicId = "4"
+                    },
+                    new Photo()
+                    {
+                        Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/AsusTek_logo.svg/1280px-AsusTek_logo.svg.png",
+                        Description = "Asus Logo",
+                        DateAdded = DateTime.Now,
+                        PublicId = "5"
+                    },
+                    new Photo()
+                    {
+                        Url = "https://upload.wikimedia.org/wikipedia/en/1/1f/2019-msi-logo.png",
+                        Description = "MSI Logo",
+                        DateAdded = DateTime.Now,
+                        PublicId = "6"
+                    },
+                    new Photo()
+                    {
+                        Url = "https://upload.wikimedia.org/wikipedia/hr/c/cc/400px-Nvidia_logo.svg.png",
+                        Description = "Nvidia Logo",
+                        DateAdded = DateTime.Now,
+                        PublicId = "7"
+                    }, 
+                    new Photo()
+                    {
+                        Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/NZXT.svg/1280px-NZXT.svg.png",
+                        Description = "NZXT Logo",
+                        DateAdded = DateTime.Now,
+                        PublicId = "8"
+                    },
+                    new Photo()
+                    {
+                        Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/EVGA_Logo.svg/1280px-EVGA_Logo.svg.png",
+                        Description = "EVGA Logo",
+                        DateAdded = DateTime.Now,
+                        PublicId = "9"
+                    },
+                    new Photo()
+                    {
+                        Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Samsung_wordmark.svg/1280px-Samsung_wordmark.svg.png",
+                        Description = "Samsung Logo",
+                        DateAdded = DateTime.Now,
+                        PublicId = "10"
+                    },
+                    new Photo()
+                    {
+                        Url = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Kingston_Technology_logo.svg/1280px-Kingston_Technology_logo.svg.png",
+                        Description = "Kingston Logo",
+                        DateAdded = DateTime.Now,
+                        PublicId = "11"
+                    },
+                    new Photo()
+                    {
+                        Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Micron_Technology_logo.svg/1280px-Micron_Technology_logo.svg.png",
+                        Description = "Micron Logo",
+                        DateAdded = DateTime.Now,
+                        PublicId = "12"
+                    },
+                    new Photo()
+                    {
+                        Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Cooler_Master_black_logo.svg/1024px-Cooler_Master_black_logo.svg.png",
+                        Description = "Cooler Master Logo",
+                        DateAdded = DateTime.Now,
+                        PublicId = "13"
+                    }
                 );
             }
             else
@@ -69,18 +153,18 @@ namespace PC_Building_Application.Helper
                 Console.WriteLine("Adding the most popular Manufacturers to the database!");
                 context.AddRange
                 (
-                    new Manufacturer() { Name = "AMD", City = "Santa Clara, CA", Country = "U.S.A"},
-                    new Manufacturer() { Name = "Intel", City = "Santa Clara, CA", Country = "U.S.A" },
-                    new Manufacturer() { Name = "Asus", City = "Taipei", Country = "Taiwan" },
-                    new Manufacturer() { Name = "MSI", City = "New Taipei", Country = "Taiwan" },
-                    new Manufacturer() { Name = "Nvidia", City = "Santa Clara, CA", Country = "U.S.A" },
-                    new Manufacturer() { Name = "NZXT", City = "City of Industry, CA", Country = "U.S.A" },
-                    new Manufacturer() { Name = "EVGA Corporation", City = "Santa Clara, CA", Country = "U.S.A" },
-                    new Manufacturer() { Name = "Samsung", City = "Seoul", Country = "South Korea" },
-                    new Manufacturer() { Name = "Kingston Technology", City = "Fountain Valley, CA", Country = "U.S.A" },
-                    new Manufacturer() { Name = "Micron Technology", City = "Boise, ID", Country = "U.S.A" },
-                    new Manufacturer() { Name = "Cooler Master", City = "New Taipei", Country = "Taiwan" },
-                    new Manufacturer() { Name = "Other", City = "N/A", Country = "N/A" }
+                    new Manufacturer() { Name = "AMD", City = "Santa Clara, CA", Country = "U.S.A", PhotoId = 3},
+                    new Manufacturer() { Name = "Intel", City = "Santa Clara, CA", Country = "U.S.A", PhotoId = 4 },
+                    new Manufacturer() { Name = "Asus", City = "Taipei", Country = "Taiwan", PhotoId = 5 },
+                    new Manufacturer() { Name = "MSI", City = "New Taipei", Country = "Taiwan", PhotoId = 6 },
+                    new Manufacturer() { Name = "Nvidia", City = "Santa Clara, CA", Country = "U.S.A", PhotoId = 7 },
+                    new Manufacturer() { Name = "NZXT", City = "City of Industry, CA", Country = "U.S.A", PhotoId = 8 },
+                    new Manufacturer() { Name = "EVGA Corporation", City = "Santa Clara, CA", Country = "U.S.A", PhotoId = 9 },
+                    new Manufacturer() { Name = "Samsung", City = "Seoul", Country = "South Korea", PhotoId = 10 },
+                    new Manufacturer() { Name = "Kingston Technology", City = "Fountain Valley, CA", Country = "U.S.A", PhotoId = 11 },
+                    new Manufacturer() { Name = "Micron Technology", City = "Boise, ID", Country = "U.S.A", PhotoId = 12 },
+                    new Manufacturer() { Name = "Cooler Master", City = "New Taipei", Country = "Taiwan", PhotoId = 13 },
+                    new Manufacturer() { Name = "Other", City = "N/A", Country = "N/A", PhotoId = 2 }
                 );
             }
             else
@@ -102,6 +186,16 @@ namespace PC_Building_Application.Helper
             else
             {
                 Console.WriteLine("There are some storage types in the database!");
+            }
+
+            if (!context.Users.Any())
+            {
+                Console.WriteLine("Adding default user to the database!");
+                context.Users.Add(new User() { UserName = "Anonymous user", Email = "anonymous@buildit.com", EmailConfirmed = true, PhotoId = 1 });
+            }
+            else
+            {
+                Console.WriteLine("There are some users in the database!");
             }
 
             context.SaveChanges();
