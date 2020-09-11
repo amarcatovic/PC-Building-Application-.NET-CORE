0330804b4a371c58c0f086bb0f45303feb01d9fd
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-11 15:27:38 +0200

    add interface for join class repo

---
+++ PC Building Application/Data/Repositories/Interfaces/IPCRepo.cs
@@ -0,0 +1,15 @@
+﻿using PC_Building_Application.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories.Interfaces
+{
+    public interface IPCRepo
+    {
+        Task CreatePCInitial(PC pc);
+        Task<PC> GetPCById(int id);
+        Task<PC> CreatePCWithParts(int id, PC pc);
+    }
+}
