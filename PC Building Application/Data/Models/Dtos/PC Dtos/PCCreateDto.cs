469535f55b3f5dbf4dbee5144c34db2563eeee76
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-11 17:51:32 +0200

    Reimplement dto, change arhitecture

--- PC Building Application/Data/Models/Dtos/PC Dtos/PCCreateDto.cs
+++ PC Building Application/Data/Models/Dtos/PC Dtos/PCCreateDto.cs
@@ -17,5 +17,13 @@ namespace PC_Building_Application.Data.Models.Dtos.PC_Dtos
         public string BuildDescription { get; set; }
         public DateTime DateBuilt { get; set; }
         public string UserId { get; set; }
+        public int MotherboardId { get; set; }
+        public int CPUId { get; set; }
+        public int GPUId { get; set; }
+        public int CoolerId { get; set; }
+        public int PowerSupplyId { get; set; }
+        public int CaseId { get; set; }
+        public ICollection<int> ramIds { get; set; }
+        public ICollection<int> storageIds { get; set; }
     }
 }
