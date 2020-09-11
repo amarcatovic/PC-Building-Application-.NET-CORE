5a8b1ad06b9a148075f8138bf163ec5df7d39327
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-11 15:27:23 +0200

    add pc read dto

---
+++ PC Building Application/Data/Models/Dtos/PC Dtos/PCReadDto.cs
@@ -0,0 +1,31 @@
+﻿using PC_Building_Application.Data.Models.Dtos.Case_Dtos;
+using PC_Building_Application.Data.Models.Dtos.Cooler_Dtos;
+using PC_Building_Application.Data.Models.Dtos.CPU_Dtos;
+using PC_Building_Application.Data.Models.Dtos.GPU_Dtos;
+using PC_Building_Application.Data.Models.Dtos.Power_Supply_Dtos;
+using PC_Building_Application.Data.Models.Dtos.RAM_Dtos;
+using PC_Building_Application.Data.Models.Dtos.Storage_Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.PC_Dtos
+{
+    public class PCReadDto
+    {
+        public int Id { get; set; }
+        public string BuildTitle { get; set; }
+        public string BuildDescription { get; set; }
+        public DateTime DateBuilt { get; set; }
+        public MotherboardReadDto Motherboard { get; set; }
+        public CPUReadDto CPU { get; set; }
+        public UserReadDto User { get; set; }
+        public GPUReadDto GPU { get; set; }
+        public CoolerReadDto Cooler { get; set; }
+        public PowerSupplyReadDto PowerSupply { get; set; }
+        public CaseReadDto Case { get; set; }
+        public ICollection<RAMReadDto> RAMs { get; set; }
+        public ICollection<StorageReadDto> Storages { get; set; }
+    }
+}
