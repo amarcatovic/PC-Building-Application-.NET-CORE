e1d167915bafbaa633b3e33f9f58adf966719a43
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-11 15:32:20 +0200

    implement join class repo

---
+++ PC Building Application/Data/Repositories/PCRamRepo.cs
@@ -0,0 +1,31 @@
+﻿using PC_Building_Application.Data.Models.Join_Classes;
+using PC_Building_Application.Data.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories
+{
+    public class PCRamRepo : IPCRamRepo
+    {
+        private readonly DataContext _context;
+
+        public PCRamRepo(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> InsertRAMIntoPC(int pcId, IEnumerable<int> ramIds)
+        {
+            foreach (var ramId in ramIds)
+            {
+                await _context.PCRAM.AddAsync(new PCRAM() { RAMId = ramId, PCId = pcId });
+            }
+
+            if ((await _context.SaveChangesAsync()) >= 0)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ PC Building Application/Data/Repositories/PCStorageRepo.cs
@@ -0,0 +1,30 @@
+﻿using PC_Building_Application.Data.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories
+{
+    public class PCStorageRepo : IPCStorageRepo
+    {
+        private readonly DataContext _context;
+
+        public PCStorageRepo(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> InsertStorageInPC(int pcId, IEnumerable<int> storageIds)
+        {
+            foreach (var storageId in storageIds)
+            {
+                await _context.PCStorage.AddAsync(new PCStorage() { StorageId = storageId, PCId = pcId });
+            }
+
+            if ((await _context.SaveChangesAsync()) >= 0)
+                return true;
+
+            return false;
+        }
+    }
+}
