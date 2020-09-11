110063d4b8c438bda6cfffe35d71d78b386962c6
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-11 15:27:43 +0200

    add interface for join class repo

---
+++ PC Building Application/Data/Repositories/Interfaces/IPCStorageRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories.Interfaces
+{
+    public interface IPCStorageRepo
+    {
+        Task<bool> InsertStorageInPC(int pcId, IEnumerable<int> storageIds);
+    }
+}
