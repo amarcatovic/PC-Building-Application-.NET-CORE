a937f8170b06c1e754867a7ebcec98cfd8e11875
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-11 15:27:05 +0200

    add dto for pc initial creation

---
+++ PC Building Application/Data/Models/Dtos/PC Dtos/PCCreateDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Dtos.PC_Dtos
+{
+    public class PCCreateDto
+    {
+        public PCCreateDto()
+        {
+            UserId = "anonymous";
+            DateBuilt = DateTime.Now;
+        }
+
+        public string BuildTitle { get; set; }
+        public string BuildDescription { get; set; }
+        public DateTime DateBuilt { get; set; }
+        public string UserId { get; set; }
+    }
+}
