ab76495b0378d57b59144d93f45101c6ad2c46fd
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-12 19:25:18 +0200

    Add join class between pc and gpu

---
+++ PC Building Application/Data/Models/Join Classes/PCGPU.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Models.Join_Classes
+{
+    public class PCGPU
+    {
+        public PC PC { get; set; }
+        public int PCID { get; set; }
+        public GPU GPU { get; set; }
+        public int GPUId { get; set; }
+        public DateTime Inserted { get; set; }
+    }
+}
