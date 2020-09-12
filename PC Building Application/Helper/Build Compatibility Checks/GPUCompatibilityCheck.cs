770b9fb0426c5c7f981898427afc971facca891e
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-12 16:17:43 +0200

    Add validation methods

---
+++ PC Building Application/Helper/Build Compatibility Checks/GPUCompatibilityCheck.cs
@@ -0,0 +1,54 @@
+﻿using PC_Building_Application.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Helper.Build_Compatibility_Checks
+{
+    public class GPUCompatibilityCheck
+    {
+        public List<string> CheckGpuAndPowerSupplyCompatibility(GPU gpu, PowerSupply powerSupply)
+        {
+            var errorStrings = new List<string>();
+            if (powerSupply.NoOfPCIe6Pins < gpu.NoOfPCIe6Pins ||
+                powerSupply.NoOfPCIe8Pins < gpu.NoOfPCIe8Pins ||
+                (powerSupply.NoOfPCIe8Pins < 2 * gpu.NoOfPCIe12Pins && powerSupply.NoOfPCIe12Pins < gpu.NoOfPCIe12Pins))
+            {
+                string tempErrorString = "GPU requires (";
+                if (gpu.NoOfPCIe6Pins > 0)
+                    tempErrorString += gpu.NoOfPCIe6Pins.ToString() + ") 6 Pin Connector";
+                if (gpu.NoOfPCIe6Pins > 1)
+                    tempErrorString += "s";
+
+                if (gpu.NoOfPCIe8Pins > 0)
+                    tempErrorString += ", (" + gpu.NoOfPCIe8Pins.ToString() + ") 8 Pin Connector";
+                if (gpu.NoOfPCIe8Pins > 1)
+                    tempErrorString += "s";
+
+                if (gpu.NoOfPCIe12Pins > 0)
+                    tempErrorString += " and (" + gpu.NoOfPCIe12Pins.ToString() + ") 12 Pin Connector";
+                if (gpu.NoOfPCIe12Pins > 1)
+                    tempErrorString += "s";
+
+                tempErrorString += ", but Power supply has ";
+                if (powerSupply.NoOfPCIe6Pins > 0)
+                    tempErrorString += $"({powerSupply.NoOfPCIe6Pins}) 6 Pin";
+                if (powerSupply.NoOfPCIe8Pins > 0)
+                    tempErrorString += $", ({powerSupply.NoOfPCIe8Pins}) 8 Pin";
+                if (powerSupply.NoOfPCIe12Pins > 0)
+                    tempErrorString += $", ({powerSupply.NoOfPCIe12Pins}) 12 Pin";
+
+                if (powerSupply.NoOfPCIe6Pins == 0 && powerSupply.NoOfPCIe8Pins == 0 && powerSupply.NoOfPCIe12Pins == 0)
+                    tempErrorString += "no PCIe Pins";
+                else
+                    tempErrorString += " PCIe connectors";
+
+
+                errorStrings.Add(tempErrorString);
+            }
+
+            return errorStrings;
+        }
+    }
+}
---
+++ PC Building Application/Helper/Build Compatibility Checks/PowerSupplyCompatibilityChecks.cs
@@ -0,0 +1,30 @@
+﻿using PC_Building_Application.Data;
+using PC_Building_Application.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Helper.Build_Compatibility_Checks
+{
+    public class PowerSupplyCompatibilityChecks
+    {
+        public List<string> CheckPsuAndStoragesompatibility(PowerSupply psu, IEnumerable<Storage> storages)
+        {
+            var errorStrings = new List<string>();
+            int noOfSataCablesRequired = 0;
+
+            foreach (var storage in storages)
+            {
+                if (storage.Name != "M.2 SSD")
+                    ++noOfSataCablesRequired;
+            }
+
+            if (psu.NoOfSATACables < noOfSataCablesRequired)
+                errorStrings.Add($"There are {noOfSataCablesRequired} SATA drives, but Power supply has only {psu.NoOfSATACables} SATA power cables");
+
+
+            return errorStrings;
+        }
+    }
+}
