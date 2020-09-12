78a48b14d776630ee541fed0e8690adea1f23a54
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-12 19:25:41 +0200

    Add repo for join class

---
+++ PC Building Application/Data/Repositories/Interfaces/IPCGPURepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PC_Building_Application.Data.Repositories.Interfaces
+{
+    public interface IPCGPURepo
+    {
+        Task<bool> InsertGPUntoPC(int pcId, IEnumerable<int> gpuIds);
+        Task AddGpu(int pcId, int gpuId);
+        Task RemoveGpu(int pcId, int gpuId);
+    }
+}
